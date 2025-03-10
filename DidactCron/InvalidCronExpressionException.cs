705fc719e6a37b64382c7e3e4b80fa88dd4f4b68
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-03-10 02:34:22 -0500

    Created new exception for invalid cron express

---
+++ DidactCron/InvalidCronExpressionException.cs
@@ -0,0 +1,17 @@
+﻿namespace DidactCron
+{
+    [Serializable]
+    public class InvalidCronExpressionException : Exception
+    {
+        public InvalidCronExpressionException()
+        { }
+
+        public InvalidCronExpressionException(string message)
+            : base(message)
+        { }
+
+        public InvalidCronExpressionException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
