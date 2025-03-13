10e022f46102a4dbcf4b4205869fe63afe2eddd8
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-03-13 03:36:38 -0500

    Added XML markup and added nullable string to CronValidator class

--- DidactCron/CronValidator.cs
+++ DidactCron/CronValidator.cs
@@ -2,12 +2,18 @@
 {
     public static class CronValidator
     {
-        public static bool IsValid(string expression, out string errorMessage)
+        /// <summary>
+        /// Checks the validity of a CRON expression and outputs an exception if invalid.
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <param name="errorMessage"></param>
+        /// <returns></returns>
+        public static bool IsValid(string expression, out string? errorMessage)
         {
             try
             {
                 _ = new CronExpression(expression);
-                errorMessage = string.Empty;
+                errorMessage = null;
                 return true;
             }
             catch (Exception ex)
